84bcb990f24ad9cb014ca1396b2e99493c968bd5
Snehasish Chowdhury <snehasish.chowdhury@molgroup.com>
2021-02-10 09:37:12 +0530

    Initial commit

---
+++ DbConnect/DbConnectDac.cs
@@ -0,0 +1,51 @@
+﻿using DbConnect.DbContext;
+//using Org.Data.AdoExtensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DbConnect
+{
+    public class DbConnectDac
+    {
+        public DbConnectDac()
+        { }
+
+        public IEnumerable<HistoryCalTemp> GetDbData(string targetDate)
+        {
+            BaseDataAccess access = new BaseDataAccess();
+
+            List<DbParameter> paramList = new List<DbParameter>();
+            paramList.Add(new SqlParameter() { ParameterName = "@Dt", Value = targetDate });
+            DbDataReader reader = access.GetDataReader("GetTempByDate", paramList);
+            List<HistoryCalTemp> valList = new List<HistoryCalTemp>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    HistoryCalTemp tempData = new HistoryCalTemp();
+                    tempData.Dt = Convert.ToDateTime(reader.GetValue(reader.GetOrdinal("Date")));//("Date", DateTime.Now, Convert.ToDateTime);
+                    tempData.High = Convert.ToDecimal(reader.GetValue(reader.GetOrdinal("HighestTemp")));//("HighestTemp", 0, Convert.ToDecimal);
+                    tempData.Low = Convert.ToDecimal(reader.GetValue(reader.GetOrdinal("LowestTemp")));//("LowestTemp", 0, Convert.ToDecimal);
+                    valList.Add(tempData);
+                }
+                reader.Close();
+            }           
+            return valList;
+        }
+
+    }
+
+    public class HistoryCalTemp
+    {
+
+        public DateTime Dt { get; set; }
+
+        public Decimal High { get; set; }
+
+        public Decimal Low { get; set; }
+    }
+    
+}
